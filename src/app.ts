9d8a0fad55de29c89cc22c76c55bb04a9b00b13d
Rahat73 <rahat.ashik.18@gmail.com>
2024-06-12 15:16:47 +0600

    eslint prettier setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import { Application, Request, Response } from "express";
-import express from "express";
-import cors from "cors";
+import { Application, Request, Response } from 'express';
+import express from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  const test = "Hello World!";
+app.get('/test', async (req: Request, res: Response) => {
+  const test = 'Welcome to the Bike Rental Reservation System';
   res.send(test);
 });
 
