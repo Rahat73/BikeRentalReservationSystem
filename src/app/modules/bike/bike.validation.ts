b5fbb96851172c3e5fa999b15e3ddd1b13936426
Rahat73 <rahat.ashik.18@gmail.com>
2024-06-13 12:41:00 +0600

    interface, validation , model for bike

---
+++ src/app/modules/bike/bike.validation.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const createBikeValidationSchema = z.object({
+  body: z.object({
+    name: z.string().min(1, 'Name is required'),
+    description: z.string().min(1, 'Description is required'),
+    pricePerHour: z
+      .number()
+      .positive('Price per hour must be a positive number'),
+    isAvailable: z.boolean().optional().default(true),
+    cc: z.number().positive('CC must be a positive number'),
+    year: z.number().int().positive('Year must be a positive integer'),
+    model: z.string().min(1, 'Model is required'),
+    brand: z.string().min(1, 'Brand is required'),
+  }),
+});
+
+export const BikeValidations = {
+  createBikeValidationSchema,
+};
